f512078ad667eef3d90c3b06d149877adab2da75
António Camarão <camaraoacsn@gmail.com>
2022-08-04 12:17:14 +0100

    Root for colors created, form in progress

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  
+<form>
+  <div class="mb-6">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+    <input type="email" id="email" class="border border-gray-500 text-neutral-900 text-sm rounded-lg w-60">
+  </div>
+  <div class="mb-6">
+    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+    <input type="password" id="password" class="border border-gray-500 text-neutral-900 text-sm rounded-lg w-60">
+  </div>
+  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+
+</template>
+
+<style>
+
+</style>
