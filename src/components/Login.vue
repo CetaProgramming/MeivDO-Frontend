16e0e44993fc5454bd257254b9a42a40c531d0af
António Camarão <camaraoacsn@gmail.com>
2022-08-04 12:45:04 +0100

    MeivColors added with tailwind, not root, form in progress

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,11 +9,16 @@
     <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
     <input type="password" id="password" class="border border-gray-500 text-neutral-900 text-sm rounded-lg w-60">
   </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <button type="submit" class="btn btn-red">Submit</button>
 </form>
 
 </template>
 
 <style>
-
+.btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+.btn-red {
+    background-color: var(red);
+  }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'MeivRed' : '#CE0E2D',
+      'MeivGreen':'#006857',
+      'MeivDarkGray' : '#1D1D1E',
+      'MeivYellow' : '#FBB03B',
+      'MeivGray' : '#808080',
+      'MeivLightGray' : '#838383',
+    },
     extend: {},
   },
   plugins: [],
