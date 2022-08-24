98a973a0d26f9003d4ec7efa82f80477e944389d
bmgcosta <bmgcosta@outlook.com>
2022-08-24 12:40:07 +0100

    PROJFINAL-39 Re-struct html of the form login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="grid grid-rows-1fr-auto lg:grid-cols-2 lg:grid-rows-none h-screen">
-    <div class="image h-full">
-      <img src="../assets/christopher-burns-Wiu3w-99tNg-unsplash.jpg" alt="worker" class="h-full w-full object-cover">
-      <div class="company">
-      </div>
+  <div class="grid items-center grid-rows-1fr-auto lg:grid-cols-1fr-auto lg:grid-rows-none min-h-screen bg-image p-12">
+    <div class="flex flex-col gap-3 max-w-4/5">
+      <h1 class="text-4xl text-white">Welcome</h1>
+      <p class="text-white font-openSans">Here you can manage your tools, projects, inspections and reparations, and mutch more!</p>
     </div>
-    <div class="py-5 bg-white flex flex-col justify-center">
+    <div class="p-10 rounded h-fit bg-white flex flex-col justify-center gap-6">
       <Logo/>
-      <form id="form" class="bg-white z-10 py-10 flex flex-col items-center gap-5">  
-          <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
-          <input type="email" id="email" class="border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3" placeholder="example@email.com">
-          <label for="password" class="block text-sm font-medium text-gray-900">Password</label>
-          <input type="password" id="password" class="border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 " placeholder="*********">
-        <button type="submit" class="btn bg-MeivRed font-bold text-lg p-5 tracking-widest rounded w-80 my-7 text-white">LOGIN</button>
-        <div class="text-center">
-          <a href="">Forgot Password?</a>
-      </div>
+      <form id="form" class="bg-white z-10 flex flex-col items-center gap-5" @submit="formValid">
+          <div class="flex flex-col gap-2">
+            <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
+            <input type="email" id="email" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none" placeholder="example@email.com" @change="inputEventChange">
+            <label v-if="emailerror" class="font-openSans text-xs text-red-500">Email is required!</label>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="password" class="block text-sm font-medium text-gray-900">Password</label>
+            <input type="password" id="password" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none" placeholder="*********" @change="inputEventChange">
+            <label v-if="passworderror" class="font-openSans text-xs text-red-500">Password is required!</label>
+          </div>
+        <button type="submit" class="btn bg-MeivRed font-bold text-lg p-5 tracking-widest rounded w-80 text-white mt-5">LOGIN</button>
       </form>
     </div>
   </div>
