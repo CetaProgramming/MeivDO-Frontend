d46091007c8d08d61005a2ed232d751ceebab413
bmgcosta <bmgcosta@outlook.com>
2022-08-25 16:05:44 +0100

    PROJFINAL-39 Adapt design in mobile view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid items-center grid-rows-1fr-auto lg:grid-cols-1fr-auto lg:grid-rows-none min-h-screen bg-image p-12">
-    <div class="flex flex-col gap-3 max-w-4/5">
+  <div class="lg:p-12 grid items-center grid-rows-1fr-auto bg-image min-h-screen lg:grid-cols-1fr-auto lg:grid-rows-none">
+    <div class="flex flex-col gap-3 p-10 lg:p-0 lg:max-w-4/5">
       <h1 class="text-4xl text-white">Welcome</h1>
       <p class="text-white font-openSans">Here you can manage your tools, projects, inspections and reparations, and mutch more!</p>
     </div>
-    <div class="p-10 rounded h-fit bg-white flex flex-col justify-center gap-6">
+    <div class="p-3 rounded h-fit bg-white flex flex-col justify-center gap-6 lg:p-10">
       <Logo/>
       <form id="form" class="bg-white z-10 flex flex-col items-center gap-5" @submit="formValid">
           <div class="flex flex-col gap-2">
