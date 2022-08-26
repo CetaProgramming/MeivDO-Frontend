d8e530058b11d3692190f14d3d35a113c1849d0e
bmgcosta <bmgcosta@outlook.com>
2022-08-26 11:54:55 +0100

    PROJFINAL-42 Created component select languague and translation files

---
+++ src/components/public/Languague.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- <h5 class="text-white text-xs font-bold"></h5> -->
+    <select class="p-1" v-model="lang" @change="emitLanguagueChanged">
+        <option value="en">English</option>
+        <option value="pt">Português</option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: 'SelectLanguague',
+        mounted(){
+            if(!localStorage.getItem('lang'))
+                localStorage.setItem('lang', 'en')  
+            return this.lang = localStorage.getItem('lang');  
+        },
+        data(){
+            return {
+                lang: 'en'            
+            }
+        },
+        methods: {
+            emitLanguagueChanged(){
+                localStorage.setItem('lang', this.lang);
+                console.log(this.languag);
+                this.$emit('languagueChangedEvent', this.lang);
+            }
+        },
+        computed: {
+            langs(){
+                return import(`../../locales/${ localStorage.getItem('lang') }.json`)
+            }
+        }
+    }
+</script>
