fb044b813c2f24803f0422b803bd016e7f96b103
bmgcosta <bmgcosta@outlook.com>
2022-08-28 23:04:05 +0100

    PROJFINAL-43 Created ToastLayout Base and four type (Error, Info, Warning, Success), also add message login is wrong and file .env to storage link api

---
+++ src/components/public/Toast/ToastLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col gap-3 fixed w-full z-50 text-white p-2 text-xs font-openSans cursor-pointer lg:w-auto lg:top-2 lg:right-2" @click="closeToast">
+        <h3 class="text-center">{{ msg }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            setTimeout(() => this.closeToast(), 5000);
+        },
+        methods: {
+            closeToast(){
+                this.$emit('closeToast');
+            }
+        },
+        props: {
+            msg: String,
+            background: String
+        },
+        emits: ['closeToast']
+    }
+</script>
---
+++ src/components/public/Toast/ToastWarning.vue
@@ -0,0 +1,15 @@
+<template>
+    <ToastLayout class="bg-orange-600" :msg="msg"/>
+</template>
+
+<script>
+    import ToastLayout from './ToastLayout.vue';
+    export default {
+        components: {
+            ToastLayout
+        },
+        props: {
+            msg: String
+        }
+    }
+</script>
