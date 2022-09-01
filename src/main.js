4da649675ec11493a5a5a27d8552e507d5b3915e
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-01 19:33:44 +0100

    PROJFINAL-28 added font awesome icons to library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas} from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -9,5 +14,8 @@ const pinia = createPinia()
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
+library.add(fas)
+library.add(faUser)
+app.component('fas', FontAwesomeIcon)
 app.mount('#app')
 
