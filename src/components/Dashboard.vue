d4e3afa8913d25efe7b33ec11dd0e1b204b83af7
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-01 19:40:20 +0100

    PROJFINAL-28 created dashboard that receives all components

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,19 +1,17 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>hello dashboard</h1>
+  <div>
+    <MenuDashboard/>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script>
+import MenuDashboard from "./dashboard/MenuDashboard.vue";
+  
+export default {
+    components: { MenuDashboard }
 }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
