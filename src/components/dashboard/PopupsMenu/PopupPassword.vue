61f47f64a854608fe3e9cb84a02409109a9dfc54
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-02 17:44:53 +0100

    PROJFINAL-28 created password popup with imported languages

---
+++ src/components/dashboard/PopupsMenu/PopupPassword.vue
@@ -0,0 +1,94 @@
+<template>
+    <Popoup titlePopUp="Change Password">
+        <div class="font-openSans grid grid-cols-1  md:justify-between  md:flex-row gap-6 ">
+            <div class="flex flex-col gap-2">
+                <label for="password" class="block text-sm font-medium text-gray-900">{{ langs.Password }}</label>
+                <input type="password" id="password" v-model="formLogin.password"
+                    class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none"
+                    placeholder="*********" @change="inputValid">
+                <label v-if="errorsInputs.password" class="font-openSans text-xs text-red-500">{{ langs.PasswordError
+                }}</label>
+            </div>
+            <div class=" grid md:grid-cols-2 gap-6">
+                <div class="flex flex-col gap-2 ">
+                    <label for="password" class="block text-sm font-medium text-gray-900">{{ langs.NewPassword
+                    }}</label>
+                    <input type="password" id="Newpassword"
+                        class="font-openSans border w-full border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none"
+                        placeholder="*********" @change="inputValid">
+                </div>
+                <div class="flex flex-col gap-2 " >
+                    <label for="password" class="block text-sm font-medium text-gray-900">{{ langs.ConfirmNewPassword
+                    }}</label>
+                    <input type="password" id="Newpassword"
+                        class="font-openSans border w-full border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none"
+                        placeholder="*********" @change="inputValid">
+                </div>
+                <!-- <label class="font-openSans text-xs text-red-500">{{langs.PasswordError}}</label> -->
+
+            </div>
+        </div>
+        <button class="bg-red-500 text-white rounded-md p-2 w-full mt-4  items-center">{{ langs.SavePass }}</button>
+    </Popoup>
+</template>
+
+<script>
+import DataValidator from '../../mixins/DataValidator.js';
+import Popoup from '../../public/Popoup.vue';
+import { computed } from 'vue';
+import { langStore } from '../../../store/langStore';
+
+export default {
+    setup() {
+        const store = langStore();
+
+        const langs = computed(() => store.getLang.PopupPassword);
+
+        return {
+            langs
+        }
+    },
+    components: {
+        Popoup
+    },
+    data() {
+        return {
+            formLogin: {
+                email: '',
+                password: '',
+            },
+            errorsInputs: {
+                email: false,
+                password: false
+            }
+        }
+    },
+    methods: {
+        languageChanged() {
+            SelectLanguague.computed.langs()
+                .then(data => this.langs = data.Login);
+        },
+        inputValid(event) {
+            if (this.empty(this.formLogin[event.target.id]))
+                this.errorsInputs[event.target.id] = true;
+        },
+        formValid(event) {
+            try {
+                let isError = false;
+                const dataForm = JSON.parse(JSON.stringify(this.formLogin));
+                Object.entries(dataForm).forEach(([key, value]) => {
+                    if (!value) {
+                        this.errorsInputs[key] = true;
+                        isError = true;
+                    }
+                });
+                if (!isError)
+                    console.log('RequestAPI');
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    },
+    mixins: [DataValidator]
+}
+</script>
