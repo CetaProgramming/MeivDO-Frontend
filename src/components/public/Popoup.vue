c4a5e16d1f5e79efc22fef458434594a6a42e0f2
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-02 17:43:54 +0100

    PROJFINAL-28 build popup and imported language store

--- src/components/public/Popoup.vue
+++ src/components/public/Popoup.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="absolute bg-black bg-opacity-30 flex justify-center items-center min-h-screen top-0 w-screen">
-        <div class="md:w-3/5 w-full min-h-screen md:min-h-fit  bg-yellow-300  " >
-            <div>
-                <div>
-                    <h1>{{titlePopUp}}</h1>
-                 <button @click="closePopUp">x</button>
+        <div class="md:w-3/5 w-full min-h-screen md:min-h-fit  bg-white rounded-md " >
+            <div class="p-10 flex flex-col gap-4 ">
+                <div class="flex justify-between ">
+                    <h1 class="text-2xl">{{titlePopUp}}</h1>
+                 <button class="text-3xl" @click="closePopUp">X</button>
                 </div>
             <slot></slot>
             </div>
@@ -14,11 +14,17 @@
 </template>
 
 <script>
+ import { computed } from 'vue';
+import { langStore } from '../../store/langStore';
 export default {
-    setup () {
-        
+    setup() {
+        const store = langStore();
 
-        return {}
+        const langs = computed(() => store.getLang.StandartProfile);
+
+        return {
+            langs
+        }
     },
     props:{
         titlePopUp: String
