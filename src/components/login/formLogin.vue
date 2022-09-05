8873e0ce2f8cd3e340e7310bcadd570446cfe9d6
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-05 15:48:06 +0100

    PROJFINAL-69 added dark mode to login form

--- src/components/login/formLogin.vue
+++ src/components/login/formLogin.vue
@@ -1,14 +1,14 @@
 <template>
     <ToastError v-if="this.toast.visible" :msg="this.toast.msg" @closeToast="this.toast.visible = false"/>
-    <form id="form" class="bg-white z-10 flex flex-col items-center gap-5" @submit.prevent="formValid">
+    <form id="form" class=" z-10 flex flex-col items-center gap-5 " @submit.prevent="formValid">
         <div class="flex flex-col gap-2">
-            <label for="email" class="block text-sm font-medium text-gray-900">{{langs.EmailInput}}</label>
-            <input type="email" id="email" v-model="formLogin.email" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none" placeholder="example@email.com" @change="inputValid">
+            <label for="email" class="block text-sm font-medium text-gray-900 dark:text-white">{{langs.EmailInput}}</label>
+            <input type="email" id="email" v-model="formLogin.email" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800" placeholder="example@email.com" @change="inputValid">
             <label v-if="errorsInputs.email" class="font-openSans text-xs text-red-500">{{langs.EmailError}}</label>
         </div>
         <div class="flex flex-col gap-2">
-            <label for="password" class="block text-sm font-medium text-gray-900">{{langs.PasswordInput}}</label>
-            <input type="password" id="password" v-model="formLogin.password" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none" placeholder="*********" @change="inputValid">
+            <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white">{{langs.PasswordInput}}</label>
+            <input type="password" id="password" v-model="formLogin.password" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800" placeholder="*********" @change="inputValid">
             <label v-if="errorsInputs.password" class="font-openSans text-xs text-red-500">{{langs.PasswordError}}</label>
         </div>
         <button type="submit" class="btn bg-MeivRed font-bold text-lg p-5 tracking-widest rounded w-80 text-white mt-5">{{langs.LoginButton}}</button>
