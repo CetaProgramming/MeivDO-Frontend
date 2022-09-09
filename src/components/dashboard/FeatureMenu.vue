4b0ac524ec9e9d278f3da27a302ebf1a4669ae24
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-09 12:03:36 +0100

    PROJFINAL-27  implemented logic  to filter in all language

--- src/components/dashboard/FeatureMenu.vue
+++ src/components/dashboard/FeatureMenu.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="grid grid-cols-1 auto-rows-max  bg-MeivAsh gap-4 p-16 md:grid-cols-2 lg:grid-cols-4 dark:bg-zinc-900">
-        <ItemMenu v-for="item in (itemsSelector.length && itemsSelector || menuItems  )" :img="langs[item].img" :title="langs[item].title"
-            :description="langs[item].description" />
+        <ItemMenu v-if="(inputedText && itemsSelector.length) || !inputedText "
+            v-for="item in (itemsSelector.length && itemsSelector || menuItems  )" :img="langs[item].img"
+            :title="langs[item].title" :description="langs[item].description" />
+        <h1 class="font-openSans col-start-1 col-end-5 text-center dark:text-white " v-else>{{langs.noItemFound}} <span
+                class="text-red-600  ">{{inputedText}}</span> </h1>
     </div>
+
 </template>
 
 <script>
@@ -26,6 +30,7 @@ export default {
     data() {
 
         return {
+            inputedText: '',
             itemsSelector: []
         }
     },
@@ -34,14 +39,15 @@ export default {
     },
     mounted() {
         this.emitter.on("filterItemMenu", inputValue => {
-            this.itemsSelector=  this.menuItems.filter(item => item.includes(inputValue.toLowerCase()))
+            this.inputedText = inputValue
+            this.itemsSelector = this.menuItems.filter(item => this.langs[item].title.toLowerCase().includes(inputValue.toLowerCase()))
         });
     },
-    beforeUnmount() {  
-             this.emitter.$off('filterItemMenu') 
-         },
-    computed:{
-        menuItemsFilter(){
+    beforeUnmount() {
+        this.emitter.$off('filterItemMenu')
+    },
+    computed: {
+        menuItemsFilter() {
 
         }
     }
