b2406e27bb2e76487a56f588a9da6c7ae0772892
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-09 21:31:33 +0100

    PROJFINAL-86 added condition to apear or not bottom menu in pages and andded link to props in ItemMenu

--- src/components/dashboard/ItemMenu.vue
+++ src/components/dashboard/ItemMenu.vue
@@ -1,4 +1,5 @@
 <template>
+    <router-link :to="{name:link}">
     <div class="group text-center p-10 md:max-w-sm max-w bg-white rounded-lg border-2 duration-200 font-openSans cursor-pointer
      hover:bg-red-300 hover:border-solid hover:border-red-500 h-fit dark:bg-black dark:border-zinc-900 dark:hover:border-red-700 dark:hover:bg-MeivRedDark">
     <div class="flex justify-center">
@@ -7,12 +8,13 @@
         <h1 class="pt-4 mb-2 text-xl font-bold text-black dark:text-white">{{title}}</h1>
         <p class="md:text-base text-gray-400 group-hover:text-black pt-2 text-sm dark:group-hover:text-white">{{description}}</p>
     </div>
+    </router-link>
 </template>
 
 <script>
 
     export default {
-        props: ['img', 'title', 'description']
+        props: ['img', 'title', 'description', 'link']
     }
 
 </script>
