0b9a45d179b9fff4e5747e9953933491a57f9f92
bmgcosta <bmgcosta@outlook.com>
2022-09-12 22:42:54 +0100

    PROJFINAL-99 Created new mixIn FormValidate to validate if any input empty in form and yes show the error

---
+++ src/components/mixins/FormValidate.js
@@ -0,0 +1,17 @@
+import { ref } from "vue";
+
+export default{
+    methods: {
+        validateData(data, refs){
+            const error = ref(true);
+            const dataForm = JSON.parse(JSON.stringify(data));
+            Object.entries(dataForm).forEach(([key, value]) => {
+                if(!value){
+                    refs[key].isError = true;
+                    error.value = false;
+                }
+            });
+            return error.value;
+        }
+    }
+}
