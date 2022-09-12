5cdb60b780c48312b476b0a1fa0d0da8dab877eb
bmgcosta <bmgcosta@outlook.com>
2022-09-12 22:44:55 +0100

    PROJFINAL-99 Refactoring Login Form to use widgets

--- src/components/login/formLogin.vue
+++ src/components/login/formLogin.vue
@@ -1,26 +1,22 @@
 <template>
     <ToastError v-if="this.toast.visible" :msg="this.toast.msg" @closeToast="this.toast.visible = false"/>
     <form id="form" class=" z-10 flex flex-col items-center gap-5 " @submit.prevent="formValid">
-        <div class="flex flex-col gap-2">
-            <label for="email" class="block text-sm font-medium text-gray-900 dark:text-white">{{langs.EmailInput}}</label>
-            <input type="email" id="email" v-model="formLogin.email" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800" placeholder="example@email.com" @change="inputValid">
-            <label v-if="errorsInputs.email" class="font-openSans text-xs text-red-500">{{langs.EmailError}}</label>
-        </div>
-        <div class="flex flex-col gap-2">
-            <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white">{{langs.PasswordInput}}</label>
-            <input type="password" id="password" v-model="formLogin.password" class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm w-80 p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800" placeholder="*********" @change="inputValid">
-            <label v-if="errorsInputs.password" class="font-openSans text-xs text-red-500">{{langs.PasswordError}}</label>
-        </div>
-        <button type="submit" class="btn bg-MeivRed font-bold text-lg p-5 tracking-widest rounded w-80 text-white mt-5">{{langs.LoginButton}}</button>
+        <InputFull ref="formLoginEmail" type="email" v-model="formLogin.email" placeholder="example@email.com" :msg=langs.EmailError :name="langs.EmailInput" />
+        <InputFull ref="formLoginPassword" type="password" v-model="formLogin.password" placeholder="*********" :msg=langs.PasswordError :name="langs.PasswordInput" />
+        <Button :text="langs.LoginButton" />
     </form>
 </template>
 
 <script>
-    import DataValidator from '../mixins/DataValidator.js';
+    import FormValidate from '../mixins/FormValidate.js';
     import { computed } from 'vue';
     import { langStore } from "../../store/langStore";
     import { userLogin } from "../../store/userLogin";
     import ToastError from "./../public/Toast/ToastError.vue";
+    import Input from '../widgets/Input.vue';
+    import InputFull from '../widgets/InputFull.vue';
+    import Button from '../widgets/Button.vue';
+    import DataManipulate from "./../../helpers/DataManipulate"
     
     export default {
         setup(){
@@ -43,34 +39,17 @@
                 formLogin: {
                     email: '',
                     password: '',
-                },
-                errorsInputs: {
-                    email: false,
-                    password: false
                 }
             }
         },
         methods: {
-            inputValid(event){
-                if(this.empty(this.formLogin[event.target.id]))
-                    return this.errorsInputs[event.target.id] = true;
-                return this.errorsInputs[event.target.id] = false;
-            },
             formValid(){
-                try {
-                    let isError = false;
-                    const dataForm = JSON.parse(JSON.stringify(this.formLogin));
-                    Object.entries(dataForm).forEach(([key, value]) => {
-                        if(!value){
-                            this.errorsInputs[key] = true;
-                            return isError = true;
-                        }
-                        return this.errorsInputs[key] = false;
-                    });
-                    if(!isError)
+                
+                try {    
+                    if(this.validateData(this.formLogin, { email: this.$refs.formLoginEmail , password:  this.$refs.formLoginPassword }))
                         (async () => {
                             try {
-                                await this.userLoginStore.login(dataForm);
+                                await this.userLoginStore.login(DataManipulate.formData(this.formLogin));
                                 this.$router.push("/dashboard");
                             } catch (error) {
                                 this.toast.msg = this.langs.LoginWrong;
@@ -84,8 +63,11 @@
             }
         },
         components: {
-            ToastError
+            ToastError,
+            Input,
+            InputFull,
+            Button
         },
-        mixins: [ DataValidator ]
+        mixins: [ FormValidate ]
     }
 </script>
