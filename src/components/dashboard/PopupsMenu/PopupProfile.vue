5dc62e2cc650711423260c7cf86e1f957cacfc7b
bmgcosta <bmgcosta@outlook.com>
2022-09-13 15:01:08 +0100

    PROJFINAL-99 Refactoring popUpProfile to use components form

--- src/components/dashboard/PopupsMenu/PopupProfile.vue
+++ src/components/dashboard/PopupsMenu/PopupProfile.vue
@@ -1,15 +1,9 @@
 <template>
     <Popoup :titlePopUp="langs.Title1">
-    <form @submit.prevent="updateProfile">
-        <div class="font-openSans grid grid-cols-1 items-center gap-5 md:gap-1 md:grid-cols-2  md:grid-cols-1fr-auto md:justify-between  md:flex-row ">
+    <form @submit.prevent="updateProfile" class="flex flex-col gap-5">
+        <div class="font-openSans grid grid-cols-1 items-center gap-5 md:gap-1  md:grid-cols-1fr-auto md:justify-between  md:flex-row ">
         <div class="flex flex-col gap-7">
-            <div class="flex flex-col gap-2">
-                <label class="font-bold">{{langs.Name}}</label>
-                <!-- necessário ir a base de dados buscar os dados de utilizador -->
-                <input id="name" class="p-3 rounded-md text-black text-sm bg-gray-300 pl-2 placeholder-black dark:placeholder-white dark:text-white dark:bg-zinc-800 md:w-4/5"
-                 type="text" v-model="formUpdateInfo.name" @change="inputValid"/>
-                 <label v-if="errorsInputs.name" class="font-openSans text-xs text-red-500 ">{{langs.nameError}}</label>
-            </div>
+            <InputLabelError ref="formUpdateInfoName" v-model="formUpdateInfo.name" :default="formUpdateInfo.name" class="md:w-4/5" :placeholder="langs.Name" :msg="langs.nameError" :name="langs.Name"/>
             <div class="flex flex-col gap-2">
                 <label class="font-bold">Email</label>
                 <h2 class="ml-4">{{email}}</h2>
@@ -20,19 +14,18 @@
             </div>
         </div>
             <div class="flex flex-col justify-center items-center">
-                <img class="dropdown w-28 h-28 md:w-32 md:h-32 rounded-full" @click="isMenuClicked = !isMenuClicked" :src=formUpdateInfo.image alt="f" />
+                <img class="dropdown w-28 h-28 md:w-32 md:h-32 rounded-full object-top object-cover" @click="isMenuClicked = !isMenuClicked" :src=formUpdateInfo.image alt="f" />
                 <input class="hidden" type="file" @change="changePicture" ref="fileInput">
                 <button class="bg-black text-white p-3 rounded-lg mt-2 dark:bg-zinc-800" @click.prevent="$refs.fileInput.click()">{{langs.Picture}}</button>
             </div>
 
         </div>
-        <button class="bg-red-500 text-white rounded-md p-2 w-full mt-4  items-center">{{langs.SaveProf}}</button>
+        <Button :text="langs.SaveProf" />
     </form>
     </Popoup>
 </template>
 
 <script>
-import DataValidator from '../../mixins/DataValidator.js';
 import Popoup from '../../public/Popoup.vue';
 import { computed } from 'vue';
 import { langStore } from '../../../store/langStore';
@@ -40,7 +33,9 @@ import {useDark, useToggle} from '@vueuse/core';
 import { userLogin } from '../../../store/userLogin';
 import ToastError from "../../public/Toast/ToastError.vue"
 import ToastSuccess from "../../public/Toast/ToastSuccess.vue"
-import axios from 'axios';
+import InputLabelError from '../../forms/InputLabelError.vue';
+import Button from '../../widgets/Button.vue';
+import FormValidate from '../../mixins/FormValidate';
 
 
 export default {
@@ -58,8 +53,12 @@ export default {
         }
     },
     components: {
-        Popoup,  ToastError, ToastSuccess,
-    },
+    Popoup,
+    ToastError,
+    ToastSuccess,
+    InputLabelError,
+    Button
+},
     data(){
         return {
             toast: {
@@ -80,12 +79,7 @@ export default {
         }
     },
     methods: {
-        inputValid(event) {
-            if (this.empty(this.formUpdateInfo[event.target.id]))
-                return this.errorsInputs[event.target.id] = true;
-            return this.errorsInputs[event.target.id] = false;
-        },
-        changePicture(event){
+        changePicture(){
             const selectedFile = event.target.files[0];
             if(!selectedFile.type.includes("image")){
                 this.$emit("activeToast", {
@@ -103,16 +97,13 @@ export default {
             };
             this.formUpdateInfo.selectedFile = selectedFile;
             this.formUpdateInfo.image = URL.createObjectURL(selectedFile);
-            console.log(event.target.files[0])
+            console.log(event.target.files)
         },
         updateProfile() {
             try {
-                let isError = false;
-                    if(!this.formUpdateInfo.name){
-                        this.errorsInputs.name = true;
-                        return isError = true;
-                    };
-                if(!isError)
+                if (this.validateData({name: this.formUpdateInfo.name}, {
+                        name: this.$refs.formUpdateInfoName,
+                }))
                     (async () => {
                         try {
                             await this.updateProfile(this.formUpdateInfo);
@@ -134,6 +125,6 @@ export default {
             }
         },
     },
-    mixins: [DataValidator],
+    mixins: [FormValidate],
 }
 </script>
