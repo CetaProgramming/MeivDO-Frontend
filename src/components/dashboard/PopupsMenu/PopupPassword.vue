d3062749d583f8250ec89305548cc30ffbc9e766
bmgcosta <bmgcosta@outlook.com>
2022-09-13 14:10:03 +0100

    PROJFINAL-99 Refactoring password popUp to use InputLabelError

--- src/components/dashboard/PopupsMenu/PopupPassword.vue
+++ src/components/dashboard/PopupsMenu/PopupPassword.vue
@@ -1,52 +1,29 @@
 <template>
     <Popoup :titlePopUp="langs.Title2" >
-        <form @submit.prevent="changePassword">
+        <form @submit.prevent="changePassword" class="flex flex-col gap-5">
             <div class="font-openSans grid grid-cols-1  md:justify-between  md:flex-row gap-6 ">
-                <div class="flex flex-col gap-2">
-                    <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white">{{ langs.Password }}</label>
-                    <input type="password" id="password" v-model="formResetPassword.password"
-                        class="font-openSans border border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800"
-                        placeholder="*********" @change="inputValid">
-                    <label v-if="errorsInputs.password" class="font-openSans text-xs text-red-500 ">{{langs.PasswordError
-                    }}</label>
-                </div>
+                <InputLabelError ref="formPasswordPassword" type="password" v-model="formResetPassword.password" placeholder="********" :msg="langs.PasswordError" :name="langs.Password"/>
                 <div class=" grid md:grid-cols-2 gap-6">
-                    <div class="flex flex-col gap-2 ">
-                        <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white  ">{{ langs.NewPassword
-                        }}</label>
-                        <input type="password" id="newPassword"
-                            class="font-openSans border w-full border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800 "
-                            placeholder="*********" @change="inputValid"
-                            v-model="formResetPassword.newPassword">
-                        <label v-if="errorsInputs.newPassword" class="font-openSans text-xs text-red-500 ">{{langs.newPasswordError
-                    }}</label>
-                    </div>
-                    <div class="flex flex-col gap-2 " >
-                        <label for="password" class="block text-sm font-medium text-gray-900 dark:text-white">{{ langs.ConfirmNewPassword
-                        }}</label>
-                        <input type="password" id="passwordConfirm"
-                            class="font-openSans border w-full border-gray-500 rounded text-neutral-900 text-sm  p-3 outline-none dark:bg-zinc-800 dark:text-white dark:border-zinc-800"
-                            placeholder="*********" @change="inputValid"
-                            v-model="formResetPassword.passwordConfirm">
-                        <label v-if="errorsInputs.passwordConfirm" class="font-openSans text-xs text-red-500 ">{{langs.passwordConfirmError
-                    }}</label>
-                    </div>
+                    <InputLabelError ref="formPasswordNewPassword" type="password" v-model="formResetPassword.newPassword" placeholder="********" :msg="langs.newPasswordError" :name="langs.NewPassword"/>
+                    <InputLabelError ref="formPasswordConfPassword" type="password" v-model="formResetPassword.passwordConfirm" placeholder="********" :msg="langs.passwordConfirmError" :name="langs.ConfirmNewPassword"/>
                 </div>
             </div>
-            <button class="bg-red-500 text-white rounded-md p-2 w-full mt-4  items-center">{{ langs.SavePass }}</button>
+            <Button :text="langs.SavePass"/>
         </form>
     </Popoup>
 </template>
 
 <script>
-import DataValidator from '../../mixins/DataValidator.js';
 import Popoup from '../../public/Popoup.vue';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { langStore } from '../../../store/langStore';
 import {useDark, useToggle} from '@vueuse/core'
 import ToastError from "../../public/Toast/ToastError.vue"
 import ToastSuccess from "../../public/Toast/ToastSuccess.vue"
 import {userLogin} from "../../../store/userLogin"
+import InputLabelError from '../../forms/InputLabelError.vue';
+import Button from '../../widgets/Button.vue';
+import FormValidate from '../../mixins/FormValidate';
 
 export default {
     setup() {
@@ -62,8 +39,12 @@ export default {
         }
     },
     components: {
-        Popoup, ToastError, ToastSuccess,
-    },
+    Popoup,
+    ToastError,
+    ToastSuccess,
+    InputLabelError,
+    Button
+},
     data() {
         return {
             toast: {
@@ -85,42 +66,19 @@ export default {
         }
     },
     methods: {
-        inputValid(event) {
-            if (this.empty(this.formResetPassword[event.target.id]))
-                return this.errorsInputs[event.target.id] = true;
-            return this.errorsInputs[event.target.id] = false;
-        },
-        formValid(event) {
-            try {
-                let isError = false;
-                const dataForm = JSON.parse(JSON.stringify(this.formLogin));
-                Object.entries(dataForm).forEach(([key, value]) => {
-                    if (!value) {
-                        this.errorsInputs[key] = true;
-                        isError = true;
-                    }
-                });
-                if (!isError)
-                    console.log('RequestAPI');
-            } catch (e) {
-                console.log(e);
-            }
-        },
         changePassword() {
             try {
-                let isError = false;
-                Object.entries(this.formResetPassword).forEach(([key, value]) => {
-                    if(!value){
-                        this.errorsInputs[key] = true;
-                        return isError = true;
-                    }
-                    return this.errorsInputs[key] = false;
+                const isError = ref(false);
+                isError.value = !this.validateData({password: this.formResetPassword.password, newPassword: this.formResetPassword.password}, {
+                    password: this.$refs.formPasswordPassword,
+                    newPassword: this.$refs.formPasswordNewPassword
                 });
-                if(this.formResetPassword.newPassword != this.formResetPassword.passwordConfirm){
-                        this.errorsInputs["passwordConfirm"] = true;
-                        return isError = true;
-                    }
-                if(!isError)
+                const equal = this.equalData(this.formResetPassword.newPassword, this.formResetPassword.passwordConfirm);
+                if(!equal){
+                    isError.value = true;           
+                    this.$refs.formPasswordConfPassword.$data.isError = true;
+                }
+                if(!isError.value)
                     (async () => {
                         try {
                             await this.userLoginStore.changePassword(this.formResetPassword);
@@ -141,6 +99,6 @@ export default {
             }
         },
     },
-    mixins: [DataValidator]
+    mixins: [FormValidate]
 }
 </script>
