9c4d858ac47c737686ef575a8db1b88eb8e109a3
bmgcosta <bmgcosta@outlook.com>
2022-09-13 12:40:31 +0100

    PROJFINAL-99 Refactoring dashboard into sub components, added class left-0 on popUp to fix bug blur

--- src/components/dashboard/MenuDashboard.vue
+++ src/components/dashboard/MenuDashboard.vue
@@ -1,106 +1,18 @@
 <template>
-<component :is="this.toast.type" v-if="this.toast.visible" :msg="this.toast.msg" @closeToast="this.toast.visible = false"/>
-    <div class="bg-image p-4 text-white  ">
-        <div class="flex justify-between items-center p-4">
-            <Logo class="h-fit"/>
-            <div class="relative font-openSans">
-                <img class="cursor-pointer dropdown mr-6 w-14 h-14 rounded-full object-top object-cover" @click="isMenuClicked = !isMenuClicked" :src=image alt="profile" title="Menu"/>
-                <div class="absolute mt-3 right-6 md:w-44 w-52 text-black bg-white rounded-md text-sm p-1 dark:bg-MeivMatteBlack dark:text-white before:content-[''] before:block before:absolute before:w-0 before:h-0 before:border-[150px] before:border-t-0 before:border-b-12 before:border-x-12 before:border-l-transparent before:border-r-transparent before:border-b-white dark:before:border-b-black before:right-4 before:top-[-12px]"
-                    v-if="isMenuClicked">
-                    <i class="fa-thin fa-user"></i>
-                    <p @click="isProfileClicked = !isProfileClicked" class="duration-200 mb-1 p-1 hover:cursor-pointer sm:hover:bg-slate-200 dark:sm:hover:bg-zinc-900">
-                        <fas class="mr-1" icon="fa-solid fa-user" /> {{langs.UserProf}}
-                    </p>
-                    <p @click="isPasswordClicked = !isPasswordClicked" class="duration-200 mb-1 p-1 hover:cursor-pointer sm:hover:bg-slate-200 dark:sm:hover:bg-zinc-900">
-                        <fas class="mr-1" icon="fa-solid fa-lock-open" /> {{langs.ChangePass}}
-                    </p>
-                    <p @click="isSettingsClicked = !isSettingsClicked" class="duration-200 mb-1 p-1 hover:cursor-pointer sm:hover:bg-slate-200 dark:sm:hover:bg-zinc-900">
-                        <fas class="mr-1" icon="fa-solid fa-gear" />{{langs.Settings}}
-                    </p>
-                    <p class="duration-200 mb-1 p-1 hover:cursor-pointer sm:hover:bg-slate-200 dark:sm:hover:bg-zinc-900">
-                        <fas class="mr-1" icon="fa-solid fa-headset" />{{langs.Support}}
-                    </p>
-                    <hr>
-                    <p class="duration-200 p-1 hover:cursor-pointer sm:hover:bg-slate-200 dark:sm:hover:bg-zinc-900" @click="logoutUser">{{langs.Logout}}</p>
-                </div>
-            </div>
-        </div>
-        <div v-if="showDetails" class="justify-center flex  flex-col items-center mt-24 md:mt-4  gap-2 font-openSans">
-            <h1 class="text-4xl">{{langs.HeadingMenu}}<span class="text-red-500 ">{{name}}</span></h1>
-            <h1 class="text-2xl">{{langs.HeadingMenu2}}</h1>
-            <input class="w-3/5 rounded-md text-black text-xl p-2 pl-2  dark:bg-MeivMatteBlack dark:text-white" type="text"
-                @input="filterItemMenu" :placeholder="langs.SearchFeat" />
-        </div>
+    <div class="bg-image p-4 text-white ">
+        <Header />
+        <DetailsUser/>
     </div>
-    <PopupProfile v-if="isProfileClicked" @activeToast="showToast" @closePopUp="isProfileClicked= false"/>
-    <PopupPassword v-if="isPasswordClicked" @activeToast="showToast" @closePopUp="isPasswordClicked= false"/>
-    <PopupSettings v-if="isSettingsClicked" @closePopUp="isSettingsClicked= false" />
 </template>
   
 <script>
-import Logo from '../public/Logo.vue';
-import Popoup from '../public/Popoup.vue';
-import PopupProfile from './PopupsMenu/PopupProfile.vue';
-import PopupPassword from './PopupsMenu/PopupPassword.vue';
-import PopupSettings from './PopupsMenu/PopupSettings.vue';
-import { computed } from 'vue';
-import { langStore } from '../../store/langStore';
-import { useDark, useToggle } from '@vueuse/core'
-import { userLogin } from '../../store/userLogin';
+import DetailsUser from './DetailsUser.vue';
+import Header from './Header.vue';
 
 export default {
-    setup() {
-        const store = langStore();
-        const isDark = useDark()
-        const toggleDark = useToggle(isDark)
-        const { name, image, logout } = userLogin();
-
-        const langs = computed(() => store.getLang.MenuDashboard);
-
-        return {
-            langs, toggleDark, isDark, name, image, logout
-        }
-    },
-    data() {
-        return {
-            toast: {
-                msg: '',
-                visible: false,
-                type: ''
-            },
-            isMenuClicked: false,
-            isProfileClicked: false,
-            isPasswordClicked: false,
-            isSettingsClicked: false
-
-        }
-    },
-    props: ['showDetails']
-    ,
-    methods: {
-        showToast(data){
-            this.toast.msg = data.msg;
-            this.toast.visible = true;
-            this.toast.type = data.type;
-        },
-        filterItemMenu(event) {
-            this.emitter.emit("filterItemMenu", event.target.value)
-        },
-        async logoutUser(){
-            await this.logout();
-            this.$router.push({name : 'login'});
-        }
-    }
-    ,
     components: {
-        Logo,
-        Popoup,
-        PopupProfile,
-        PopupPassword,
-        PopupSettings
-    }
+    DetailsUser,
+    Header
+}
 }
 </script>
-  
-<style lang="scss" scoped>
-</style>
--- src/components/public/Popoup.vue
+++ src/components/public/Popoup.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="popUp"
-        class="absolute  md:fixed bg-black backdrop-blur-md bg-opacity-30 flex justify-center items-center min-h-screen top-0 w-full dark:bg-opacity-70">
+        class="absolute left-0 md:fixed bg-black text-black backdrop-blur-md bg-opacity-30 flex justify-center items-center min-h-screen top-0 w-full dark:bg-opacity-70">
         <div
             class="md:w-3/5 block overflow-auto w-full min-h-screen md:min-h-fit bg-white rounded-md  h-screen md:h-fit  dark:bg-black dark:text-white">
             <div class="p-10 flex flex-col gap-4 ">
