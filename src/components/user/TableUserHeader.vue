4821616b4ff02874298468ddbb142f4325dbb588
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-15 09:32:29 +0100

    PROJFINAL-11 table user and table header
    created component to build header of table and table for users witout mobile responsive. added also pagination to bottom page

---
+++ src/components/user/TableUserHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hidden p-4 mt-6 bg-zinc-300 rounded-md  xl:grid  font-bold justify-center" :style=GetLenght>
+        <h1  v-for="item in Items" class="text-center">{{item}} </h1>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    props:['Items'],
+    computed: {
+        GetLenght() {
+            
+            return   `grid-template-columns: repeat(${this.Items.length}, minmax(0, 1fr));`
+        }
+    }
+}
+</script>
+
+
---
+++ src/components/user/UserTable.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div class="bg-white rounded-md grid grid-cols-7 p-2 mt-1 items-center">
+        <p class="text-center">1</p>
+        <p class="text-center">Administrator Administrator</p>
+        <p class="text-center" >tetesttesesttte@gmail.com</p>
+        <p class="text-center">Administrator</p>
+        <div class="flex justify-center items-center">
+            <div class="bg-green-400 rounded-xl w-3 h-3 m-1"> </div>
+            <p class="text-center">Active</p>
+        </div>
+        <p class="text-center">5 minutes ago</p>
+        <select id="status" class="bg-zinc-400 p-1 rounded-md text-center hover:cursor-pointer">
+            <option value="" disabled selected hidden>Options</option>
+            <option value="Update">Update</option>
+            <option value="ResetPassword">Reset Password</option>
+            <option value="Delete">Delete</option>
+        </select>
+    </div>
+    <div class="bg-white rounded-md grid grid-cols-7 p-2 mt-1 items-center">
+        <p class="text-center">1</p>
+        <p class="text-center">Administrator</p>
+        <p class="text-center" title="olaolaola@gmail.com">teste@gmail.com</p>
+        <p class="text-center">Administrator</p>
+        <div class="flex justify-center items-center">
+            <div class="bg-red-400 rounded-xl w-3 h-3 m-1"> </div>
+            <p class="text-center">Inactive</p>
+        </div>
+        <p class="text-center">5 minutes ago</p>
+        <select id="status" class="bg-zinc-400 p-1 rounded-md text-center hover:cursor-pointer">
+            <option value="" disabled selected hidden>Options</option>
+            <option value="Update">Update</option>
+            <option value="ResetPassword">Reset Password</option>
+            <option value="Delete">Delete</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
