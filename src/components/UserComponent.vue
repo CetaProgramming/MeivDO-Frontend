6567f2fc773be6f1b41172e5cb28147d951f4518
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-15 09:15:52 +0100

    PROJFINAL-11 creating user feature
    created component that receives app header and user component; created also component user that receives all parts of the user app

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
-            <h1>user page</h1>
+        <div class="bg-image p-4 text-white ">
+      <Header/>
+        </div>
+        <userVue></userVue>
     </div>
 </template>
 
 <script>
-export default {
-    setup () {
-        
+import MenuDashboard from './dashboard/MenuDashboard.vue';
 
-        return {}
-    }
+import userVue from './user/user.vue';
+import Header from './dashboard/Header.vue';
+
+export default {
+
+    components: {
+    userVue,
+    MenuDashboard,
+    Header
+}
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
