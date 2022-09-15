fe18d28fa2500ae8407f1bcbdd249dc094a45774
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-15 09:29:49 +0100

    PROJFINAL-11 created header for users that receives components with img, the select features and a button

---
+++ src/components/user/HeaderUser.vue
@@ -0,0 +1,33 @@
+<template>
+     <div class="flex flex-col mb-4 md:flex-row justify-between items-center gap-4">
+                <UserImageName/>
+                <div>
+                    <div class="flex gap-4 ">
+                        <SelectFeature/>
+                        <button class="bg-green-500 p-2 text-white rounded-lg">+ {{langsUser.ButtonNew}}</button>
+                    </div>
+                </div>
+            </div>
+</template>
+
+<script>
+    import UserImageName from './UserImageName.vue';
+    import SelectFeature from './SelectFeature.vue';
+    import { computed } from 'vue';
+    import {langStore} from '../../store/langStore';
+export default {
+    setup () {
+        const store = langStore();
+        const langsUser = computed(() => store.getLang.UserFeature);
+
+        return {
+            langsUser
+        }
+    },
+    components:{
+        UserImageName,
+        SelectFeature
+    }
+}
+</script>
+
