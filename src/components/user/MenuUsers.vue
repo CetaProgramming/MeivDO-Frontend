1f7fa993f9d25ec85de22493510e9422e21edfae
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-15 09:30:49 +0100

    PROJFINAL-11 created component with input and select to find specific user

---
+++ src/components/user/MenuUsers.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg-white items  grid md:grid-cols-1fr-auto-auto gap-3 p-5 rounded-md">
+        <div>
+            <p class="mb-2 text-center">Keywords</p>
+            <div class="bg-zinc-300 p-1 rounded-md">
+                <fas icon="fa-solid fa-search" />
+                <input class=" indent-2 bg-zinc-300 md:w-5/6 outline-0" type="text" >
+            </div>
+        </div>
+        <div class=" ">
+            <p class="mb-2 text-center">Status</p>
+            <select id="status" class="bg-zinc-300 p-1 rounded-md hover:cursor-pointer w-full">
+                <option value="" disabled selected hidden>Select your option</option>
+                <option value="active">Active</option>
+                <option value="disabled">disabled</option>
+            </select>
+        </div>
+        <!-- <div>
+            <p class="mb-2">More Details</p>
+            <DarkModeSwitch/>
+        </div> -->
+    </div>
+</template>
+
+<script>
+import DarkModeSwitch from '../dashboard/DarkModeSwitch.vue';
+    
+export default {
+    setup() {
+        return {};
+    },
+    components: { DarkModeSwitch }
+}
+</script>
+
