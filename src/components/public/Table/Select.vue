ee144f6627c6c5e0e1145d12b638c2748d5994be
bmgcosta <bmgcosta@outlook.com>
2022-09-17 14:27:54 +0100

    PROJFINAL-112 Created new select component

---
+++ src/components/public/Table/Select.vue
@@ -0,0 +1,18 @@
+<template>
+    <select @change="this.$emit('selectOption', value)" v-model="value" class="bg-zinc-400 p-1 rounded-md text-center hover:cursor-pointer w-full lg:w-auto">
+        <option v-for="item in selectItems" :value="item.key" :hidden=item.disabled>{{item.value}}</option>
+    </select>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                value: '',
+                id: this.num
+            }
+        },
+        props: ['selectItems', 'num'],
+        emits: ['selectOption']
+    }
+</script>
