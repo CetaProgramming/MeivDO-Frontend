3fe5089d8e50f3ab525ef80e1ae420c37aa060ca
bmgcosta <bmgcosta@outlook.com>
2022-09-19 14:25:24 +0100

    PROJFINAL-112 Added custom event SelectOption

--- src/components/public/Table/TableBody.vue
+++ src/components/public/Table/TableBody.vue
@@ -16,6 +16,7 @@ export default {
     components: {
         Select
     },
+    emits: [ 'selectOption'],
     methods: {
         selectItem(select,event){
             this.$emit('selectOption', select, event);
