e4ba80f9859749fe281bd00c6184566676aa3823
bmgcosta <bmgcosta@outlook.com>
2022-09-21 20:36:56 +0100

    PROJFINAL-133 Add comma after computed property and remove extra button

--- src/components/user/HeaderUser.vue
+++ src/components/user/HeaderUser.vue
@@ -5,7 +5,6 @@
                     <div class="flex gap-4 ">
                         <NavigateItemMenu />
                         <Button @click="isAddUserClicked = !isAddUserClicked" :text="langsUser.ButtonNew" bg="bg-green-500" class="rounded-lg"></Button>
-                        <button class="bg-green-500 p-2 text-white rounded-lg dark:bg-green-600 dark:text-black">+ {{langsUser.ButtonNew}}</button>
                     </div>
                 </div>
             </div>
@@ -23,7 +22,7 @@
           return{
               isAddUserClicked: false,
           }
-        }
+        },
         computed: {
             langsUser(){
                 return langStore().getLang.UserFeature
