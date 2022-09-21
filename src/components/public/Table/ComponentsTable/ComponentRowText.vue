7a371524251a84390f86c8a2d7cf2e6a19031fc8
bmgcosta <bmgcosta@outlook.com>
2022-09-21 20:21:15 +0100

    PROJFINAL-113 Created components to use inside Row Table

---
+++ src/components/public/Table/ComponentsTable/ComponentRowText.vue
@@ -0,0 +1,24 @@
+<template>
+    <ComponentBaseTableRow :headerItem="headerTitle">
+        <p :title="item" class="text-center truncate dark:text-white">{{item}}</p>
+    </ComponentBaseTableRow>
+</template>
+
+<script>
+    import ComponentBaseTableRow from './ComponentBaseTableRow.vue';
+    export default {
+    props: {
+        headerTitle: {
+            type: String,
+            required: true
+        },
+        item: {
+            type: String,
+            required: true
+        }
+    },
+    components: { 
+        ComponentBaseTableRow 
+    }
+}
+</script>
