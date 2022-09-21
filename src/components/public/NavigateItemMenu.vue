4161de3aa0ec2b3a7fab09b9dddf317fd5f36e92
bmgcosta <bmgcosta@outlook.com>
2022-09-21 12:20:06 +0100

    PROJFINAL-130 Added logic to render options user only has access and filter them to not show the actual page is viewing

--- src/components/public/NavigateItemMenu.vue
+++ src/components/public/NavigateItemMenu.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex gap-2 bg-white p-1 rounded-md items-center dark:bg-MeivMatteBlack dark:text-white">
         <p>{{langsUser.View}}</p>
-        <select name="features" id="features" class="bg-gray-300 rounded-md p-1 hover:cursor-pointer dark:bg-zinc-800">
-            <option >Dashboard</option>
-            <option v-for="item in itemsMenuRoutes()" :value="item">
+        <select name="features" id="features" @click="optionSelected($event)" class="bg-gray-300 rounded-md p-1 hover:cursor-pointer dark:bg-zinc-800">
+            <option value="dashboard">Dashboard</option>
+            <option v-for="item in itemsMenuRoutes()" :value="langs[item].title.toLowerCase()">
                 {{ langs[item].title }}
             </option>
         </select>
@@ -30,6 +30,15 @@ export default {
         }
     },
     methods: {
+        optionSelected(event){
+            try {
+                const optionSelected = event.target;
+                if(optionSelected.tagName?.toLowerCase() === "option")
+                    this.$router.push({ name: optionSelected.value});
+            } catch (e) {
+                console.error('Ups! Something went wrong!');
+            }
+        },
         itemsMenuRoutes(){
             return this.getAccess().filter(itemMenu => !itemMenu.includes(this.$route.name));
         }
