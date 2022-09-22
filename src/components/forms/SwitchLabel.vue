24a1a0527ea3fae41fd46ed9e8b2e0275c224ec3
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-22 16:10:25 +0100

    PROJFINAL-105 created a switch widget and adapted for darkModeSwitch and for Switch used to change active in users

---
+++ src/components/forms/SwitchLabel.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-col gap-2">
+        <Label :name="name" :msg="name" />
+        <Switch v-model="value" :default="default"/>
+    </div>
+</template>
+
+<script>
+import Label from '../widgets/Label.vue';
+import Switch from '../widgets/Switch.vue';
+export default {
+    data() {
+        return {
+            value: this.default
+        };
+    },
+    props: {
+        default: {
+            type: Boolean,
+            default: false,
+        },
+        name: {
+            default: "input",
+            type: String,
+        },
+    },
+    emits: [
+        'update:modelValue'
+    ],
+
+    components: {
+    Label,
+    Switch
+}
+}
+</script>
+
