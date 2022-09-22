a523337fb845c7bc9fc2c650834dd826693c42fe
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-22 16:14:51 +0100

    PROJFINAL-105 created loop to get all roles receiving from default the one he has

--- src/components/forms/SelectLabel.vue
+++ src/components/forms/SelectLabel.vue
@@ -4,8 +4,7 @@
         <Label :name="name" :msg="name" />
         <select id="status" v-model="value" class="bg-zinc-300 p-1 rounded-md hover:cursor-pointer w-full">
             <option value="" disabled selected hidden>Select option</option>
-            <option v-for="item in items" :value="item.value">{{item.name}}</option>
-
+            <option  v-for="item in items" :value="item.id"  >{{item.name}}</option>
         </select>
     </div>
 </template>
@@ -15,7 +14,7 @@ import Label from '../widgets/Label.vue';
 export default {
     data() {
         return {
-            value: ""
+            value: this.default
         };
     },
     props: {
