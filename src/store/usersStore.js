3c305ac61689c59176983fd279a1792182c2aa54
Ricardo <ricardo.sousa13@hotmail.com>
2022-09-23 22:24:23 +0100

    PROJFINAL-105 created method created and add user

--- src/store/usersStore.js
+++ src/store/usersStore.js
@@ -2,9 +2,10 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 
 export const usersStore = defineStore('usersStore', {
-    state: () => { 
+    state: () => {
         return {
             users: [],
+            imgProfileDefault: `${import.meta.env.VITE_API_ENDPOINT}/storage/default/default-profile.png`,
             roles: [],
             pag: {
                 actualPage: 1,
@@ -17,18 +18,18 @@ export const usersStore = defineStore('usersStore', {
         }
     },
     actions: {
-        createObj(user){
+        createObj(user) {
             return {
                 id: user.id,
                 name: user.name,
-                image: `${import.meta.env.VITE_API_ENDPOINT}/storage/${user.image}`, 
+                image: `${import.meta.env.VITE_API_ENDPOINT}/storage/${user.image}`,
                 email: user.email,
                 role: user.role,
                 active: user.active,
                 updated: user.updated_at
             }
         },
-        createViewing(user){
+        createViewing(user) {
             return {
                 id: user.id,
                 name: user.name,
@@ -38,7 +39,7 @@ export const usersStore = defineStore('usersStore', {
                 updated: user.updated,
             }
         },
-        async mount(){
+        async mount() {
             const response = await axios.get(
                 `${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users`
             );
@@ -48,68 +49,81 @@ export const usersStore = defineStore('usersStore', {
             this.perPage = response.data.per_page;
             this.pagesLoad.push(1);
             this.users = response.data.data.map(user => this.createObj(user)),
-            this.viewing = this.pageViewing(this.users)
+                this.viewing = this.pageViewing(this.users)
         },
-        pageViewing(users){
+        pageViewing(users) {
             return users.map(user => this.createViewing(user));
         },
-        async load(page){
+        async load(page) {
             try {
                 const response = await axios.get(
                     `${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users?page=${page}`
                 );
                 await response.data.data.forEach(user => !this.users.some(userVer => userVer.id.value === user.id) && this.users.push(this.createObj(user)));
-                this.users.sort((user1,user2) =>user1.id.value - user2.id.value);
+                this.users.sort((user1, user2) => user1.id.value - user2.id.value);
                 this.pagesLoad.push(page);
-                this.pagesLoad.sort((a,b) => a - b);
+                this.pagesLoad.sort((a, b) => a - b);
             } catch (error) {
                 throw error;
-            }     
+            }
         },
-        async get(page){
-            try{
-                if(page > this.pag.lastPage || page < 1)
+        async get(page) {
+            try {
+                if (page > this.pag.lastPage || page < 1)
                     throw new Error('User must be authenticated')
-                if(!this.pagesLoad.includes(page))
-                    await this.load(page);   
+                if (!this.pagesLoad.includes(page))
+                    await this.load(page);
                 const indexPage = this.pagesLoad.findIndex(pagArray => pagArray === page);
                 const firstIndex = (indexPage) * this.perPage;
                 const itemPageAtual = this.perPage * (indexPage + 1);
                 const lastIndex = itemPageAtual < this.users.length ? itemPageAtual : this.users.length;
-                this.users.sort((user1,user2) =>user1.id.value - user2.id.value);
-                this.viewing = this.pageViewing(this.users.slice(firstIndex , lastIndex));
+                this.users.sort((user1, user2) => user1.id.value - user2.id.value);
+                this.viewing = this.pageViewing(this.users.slice(firstIndex, lastIndex));
                 this.pag.actualPage = page;
-            } catch(e){
+            } catch (e) {
                 throw e;
             }
         },
-        async add(){
-            const user = {
-                id: 50,
-                name: 'John Doe',
-                email: 'john@example.com',
-                role: 'admin',
-                active: 0,
-                updated: "ddasdsad"
+        async add(formData) {
+            try {
+                const response = await axios.post(
+                    `${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users`,
+                    formData
+                );
+                this.users.push(this.createObj(response.data));
+                this.get(this.pag.actualPage);
+                return response.data
+            } catch (error) {
+                throw error
             }
-            this.users.push(this.createObj(user));
-            this.get(this.pag.actualPage);
+
+
         },
-        async resetPassword(userId){
-            try{
+        async resetPassword(userId) {
+            try {
                 await axios.put(`${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users/resetPassword/${userId}`);
                 this.users.find(user => user.id === userId).updated = new Date();
                 this.get(this.pag.actualPage);
-            } catch(error){
+            } catch (error) {
                 throw error;
             }
-            
+
         },
-        async change(){
-            this.users[0].id = 10
-            this.get(this.pag.actualPage);
+        async update(userId, formData) {
+            try {
+                const response = await axios.post(
+                    `${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users/${userId}`,
+                    formData
+                );
+                this.users[this.users.findIndex(user => user.id == userId)] = this.createObj(response.data)
+                this.get(this.pag.actualPage);
+                return response.data
+            } catch (error) {
+                throw error
+            }
+
         },
-        async getRoles(){
+        async getRoles() {
             try {
                 const response = await axios.get(
                     `${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users/roles`
@@ -119,15 +133,15 @@ export const usersStore = defineStore('usersStore', {
                 throw error
             }
         },
-        async deleteUser(userId){
-            try{
+        async deleteUser(userId) {
+            try {
                 await axios.delete(`${import.meta.env.VITE_API_ENDPOINT}/${import.meta.env.VITE_API_PREFIX}/users/${userId}`);
 
                 const userDelete = this.users.findIndex(user => user.id === userId)
-                this.users.splice(userDelete, userDelete+1)
+                this.users.splice(userDelete, userDelete + 1)
                 this.get(this.pag.actualPage);
 
-            } catch(error){
+            } catch (error) {
                 throw error;
             }
         },
