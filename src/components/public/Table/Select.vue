5347ede4d6e47c457eb1ee292687b7f278effe5c
bmgcosta <bmgcosta@outlook.com>
2022-09-24 17:30:59 +0100

    PROJFINAL-146 Fix bug select the same option more one time without select another option, added new method verify if event click is occurred in option

--- src/components/public/Table/Select.vue
+++ src/components/public/Table/Select.vue
@@ -1,5 +1,5 @@
 <template>
-    <select @change="this.$emit('selectOption', value)" v-model="value" class="bg-zinc-400 p-1 rounded-md text-center hover:cursor-pointer w-full lg:w-auto dark:bg-zinc-800 dark:text-white">
+    <select @click="optionSelected" v-model="value" class="bg-zinc-400 p-1 rounded-md text-center hover:cursor-pointer w-full lg:w-auto dark:bg-zinc-800 dark:text-white">
         <option v-for="item in selectItems" :value="item.key" :hidden=item.disabled>{{item.value}}</option>
     </select>
 </template>
@@ -9,6 +9,12 @@
         data(){
             return {
                 value: ''
+            }
+        },
+        methods: {
+            optionSelected(event){
+                if(event.target.tagName.toLowerCase().includes("option"))
+                    this.$emit('selectOption', this.value)
             }
         },
         props: ['selectItems', 'num'],
