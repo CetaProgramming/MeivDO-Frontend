b6acce228fa6dde8173fd07c07f49509b027306e
bmgcosta <bmgcosta@outlook.com>
2022-09-26 16:23:50 +0100

    PROJFINAL-114 Created new inputSearch to use in filter data

---
+++ src/components/forms/InputSearch.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flex flex-col gap-2">
+        <Label :name="name" :msg="name.Label" />
+        <div class="flex bg-zinc-300 p-1 rounded-md dark:bg-zinc-800 relative items-center">
+            <fas class="pl-1" icon="fa-solid fa-search" />
+            <Input v-model="value" :placeholder="name.Input" pad="p-1" @input="changeValue" class="indent-1 w-full" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Label from '../widgets/Label.vue';
+import Input from '../widgets/Input.vue';
+export default {
+    data() {
+        return {
+            value: ''
+        };
+    },
+    props: {
+        default: {
+            default: "",
+        },
+        name: {
+            default: "input",
+            type: String,
+        }
+    },
+    methods: {
+        changeValue(event) {
+            this.$emit('update:modelValue', event.target.value)
+        }
+    },
+
+    components: {
+    Label,
+    Input
+}
+}
+</script>
