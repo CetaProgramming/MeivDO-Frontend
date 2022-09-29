2ce53da804706c959cca642f9ff16b14594e4093
bmgcosta <bmgcosta@outlook.com>
2022-09-29 18:30:59 +0100

    PROJFINAL-172 Change event change instead of click and change value to link

--- src/components/public/NavigateItemMenu.vue
+++ src/components/public/NavigateItemMenu.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex gap-2 bg-white p-1 rounded-md items-center dark:bg-zinc-900 dark:text-white">
         <p>{{langsUser.View}}</p>
-        <select name="features" id="features" v-model="inputedText" @click="optionSelected($event)" class="capitalize bg-gray-300 rounded-md p-1 hover:cursor-pointer dark:bg-zinc-800">
-            <option disabled hidden select>{{ $route.name }}</option>
+        <select name="features" id="features" @change="optionSelected($event)" class="capitalize bg-gray-300 rounded-md p-1 hover:cursor-pointer dark:bg-zinc-800">
+            <option disabled hidden selected>{{ langs[$route.name].title }}</option>
             <option value="dashboard">Dashboard</option>
-            <option v-for="item in itemsMenuRoutes()" :value="langs[item].title.toLowerCase()" :key="langs[item].title.toLowerCase()">
+            <option v-for="item in itemsMenuRoutes()" :value="langs[item].link" :key="langs[item].title.toLowerCase()">
                 {{ langs[item].title }}
             </option>
         </select>
@@ -31,10 +31,10 @@ export default {
         }
     },
     methods: {
-        optionSelected(){
+        optionSelected(event){
             try {
                 if(this.inputedText)
-                    this.$router.push({ name: this.inputedText});
+                    this.$router.push({ name: event.target.value });
             } catch (e) {
                 console.error('Ups! Something went wrong!');
             }
