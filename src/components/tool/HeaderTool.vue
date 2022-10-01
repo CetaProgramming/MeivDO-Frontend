e4348ecf79de3bfa198881b3688d0db7b61db813
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-01 09:47:23 +0100

    PROJFINAL-161 imported components of adding and created logic to switch component when necessary

--- src/components/tool/HeaderTool.vue
+++ src/components/tool/HeaderTool.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex flex-col md:flex-row justify-between items-center gap-4">
         <PageImageName :img="langsItems.tools.img" :title="langsItems.tools.title" />
+        {{newOptions}}
         <div>
             <div class="flex gap-4 ">
                 <NavigateItemMenu />
@@ -8,20 +9,25 @@
                     pad="px-4 py-2" class="rounded-lg"></Button>
             </div>
         </div>
-    </div> 
-     <AddUpdateTool v-if="isAddToolClicked" @closePopUp="isAddToolClicked= false" />
+    </div>
+    <component v-if="isAddToolClicked" @closePopUp="isAddToolClicked= false" :is="componentFilter"></component>
 </template>
 
 <script>
 import PageImageName from '../public/Table/PageImageName.vue';
-import AddUpdateTool from './PopupsTool/AddUpdateTool.vue';
+import AddUpdateTools from './PopupsTool/AddUpdateTools.vue';
+
 import { langStore } from '../../store/langStore';
 import Button from '../widgets/Button.vue';
 import NavigateItemMenu from '../public/NavigateItemMenu.vue';
+import AddUpdateGroupTools from './PopupsTool/AddUpdateGroupTools.vue';
+import AddUpdateCategory from './PopupsTool/AddUpdateCategory.vue';
 export default {
+    props: ['newOptions'],
     data() {
         return {
-            isAddToolClicked: false,
+            popUpSelected: "Tools",
+            isAddToolClicked: false
         };
     },
     computed: {
@@ -30,14 +36,25 @@ export default {
         },
         langsItems() {
             return langStore().getLang.ItemMenu
+        },
+        componentFilter(){
+            this.popUpSelected = this.newOptions
+            return `AddUpdate${this.popUpSelected}`
+        }
+    },
+    watch:{
+        newOptions(){
+            console.log("options change")
         }
     },
     components:
     {
     PageImageName,
-    AddUpdateTool,
+    AddUpdateTools,
     Button,
-    NavigateItemMenu
+    NavigateItemMenu,
+    AddUpdateGroupTools,
+    AddUpdateCategory
 }
 }
 </script>
