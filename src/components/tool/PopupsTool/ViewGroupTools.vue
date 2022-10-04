6ddbd1787267d615f661c9cd5696a2084555e8e0
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-04 16:54:46 +0100

    PROJFINAL-161 created component of popop to view tools and grouptools

---
+++ src/components/tool/PopupsTool/ViewGroupTools.vue
@@ -0,0 +1,17 @@
+<template>
+    <Popoup></Popoup>
+</template>
+
+<script>
+import Popoup from '../../public/Popoup.vue';
+export default {
+    setup() {
+        return {};
+    },
+    components: { Popoup }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
