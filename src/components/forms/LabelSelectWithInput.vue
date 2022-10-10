98df6643b509735d3500406fa32a7d92a8b9b1e0
bmgcosta <bmgcosta@outlook.com>
2022-10-10 11:46:32 +0100

    PROJFINAL-208 Build two components to use in forms, one with message error and other not

---
+++ src/components/forms/LabelSelectWithInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex flex-col gap-2 w-full">
+        <Label :name="name" :msg="name"/>
+        <InputSelectOption v-model="value" :items="items" :default="default" :placeholder="placeholder"/>
+    </div>
+</template>
+
+<script>
+import Label from '../widgets/Label.vue';
+import InputSelectOption from '../widgets/InputSelectOption.vue';
+export default {
+    data(){
+        return {
+            value: this.default,
+        }
+    },
+    watch:{
+        value(value){
+            this.$emit("update:modelValue", value);
+        }
+    },
+    props: {
+        default: {
+            type: Number
+        },
+        name: {
+            default: "input",
+            type: String,
+        },
+        placeholder: {
+            default: "placeholder",
+            type: String,
+        },
+        items: {
+            required: true
+        }
+    },
+    emits: [
+        'update:modelValue'
+    ],
+    components: { Label, InputSelectOption }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
