e7111d78519bcc12bda54b01912af8ffb5e700b7
bmgcosta <bmgcosta@outlook.com>
2022-10-14 11:16:55 +0100

    PROJFINAL-185 Created component to filter data and show what's tools are on project

--- src/components/project/managementTools/ManagementTools.vue
+++ src/components/project/managementTools/ManagementTools.vue
@@ -1,15 +1,27 @@
 <template>
     <div class=" flex flex-col gap-3 bg-MeivGray p-5  rounded dark:bg-MeivDarkGray">
         <div class="flex gap-5">
-            <InputLabel :name="langs.Filters.CodeInput.Name" :placeholder="langs.Filters.CodeInput.Placeholder" />
-            <SelectLabel :name="langs.Filters.GroupToolsInput.Name" :placeholder="langs.Filters.GroupToolsInput.Placeholder" pad="p-3" />
+            <!-- <LabelSelectWithInputError ref="formToolCreateGroup" :name="langs.GroupTools" v-model="formToolCreateUpdate.groupTool" :default="formToolCreateUpdate.groupTool"
+                            :items="activeGroupTools" itemFilter="code"
+                        /> -->
+
+            <!-- <LabelSelectWithInput :name="langs.Filters.CodeInput.Name" v-model="formFilterToolsAvailability.tool"/> -->
+            <LabelSelectWithInput   :name="langs.Filters.GroupToolsInput.Name" 
+                                    v-model="formFilterToolsAvailability.tool" 
+                                    :placeholder="langs.Filters.GroupToolsInput.Placeholder" 
+                                    :default="formFilterToolsAvailability.tool" 
+                                    :items="activeGroupTools" 
+                                    itemFilter="code"/>
+            <!-- <InputLabel :name="langs.Filters.CodeInput.Name" :placeholder="langs.Filters.CodeInput.Placeholder" /> -->
+            <!-- <SelectLabel :name="langs.Filters.GroupToolsInput.Name" :placeholder="langs.Filters.GroupToolsInput.Placeholder" pad="p-3" /> -->
             <div class="flex gap-4 justify-end">
                 <ButtonIcon icon="fa-solid fa-magnifying-glass" text="" pad="p-3" bg="bg-blue-600" space="normal" />
                 <ButtonIcon icon="fa-solid fa-trash" @click.stop.prevent="resetValues" text=""  pad="p-3"  space="normal" />
             </div>            
         </div>
         <div>
-            <TableHeader :header=langs.TableHeader />
+            <TableHeader :header=langs.TableHeader  class="grid-cols-2 text-md p-2"/>
+            <!-- <TableBody :header=langs.TableHeader /> -->
         </div>
     </div>
 </template>
@@ -20,12 +32,28 @@ import ButtonIcon from '../../widgets/ButtonIcon.vue';
 import { langStore } from '../../../store/langStore';
 import SelectLabel from '../../forms/SelectLabel.vue';
 import TableHeader from '../../public/Table/TableHeader.vue';
+import TableBody from '../../public/Table/TableBody.vue';
+import LabelSelectWithInput from '../../forms/LabelSelectWithInput.vue';
+import {groupToolsStore} from './../../../store/groupToolsStore';
+
     export default {
-        computed: {
-            langs(){
-                return langStore().getLang.Options.ManagementTools;
+        async mounted(){
+            this.activeGroupTools = await groupToolsStore().getActiveGroupTools(1, 'all');
+        },
+        data(){
+            return {
+                activeGroupTools: [],
+                formFilterToolsAvailability: {
+                    tool: -1,
+                    groupTool: -1
+                }
             }
         },
-        components: { InputLabel, ButtonIcon, SelectLabel, TableHeader }
+        computed: {
+            langs(){
+                return langStore().getLang.PageProjects.ManagementTools;
+            }
+        },
+        components: { InputLabel, ButtonIcon, SelectLabel, TableHeader, TableBody, LabelSelectWithInput }
 }
 </script>
