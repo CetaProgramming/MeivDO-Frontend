5b8dea58b3ff0491eb69f5059d8bc27db0ed0416
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-15 16:58:50 +0100

    PROJFINAL-194 created filters component for both tables in inpsection

---
+++ src/components/inspection/filters/FilterCompleted.vue
@@ -0,0 +1,56 @@
+<template>
+    <form class="bg-white items  grid lg:grid-cols-1fr-auto gap-3 p-5 rounded-md dark:bg-MeivMatteBlack dark:text-white"
+        @submit.prevent="doSearch">
+        <div class="grid lg:grid-cols-2 gap-3">
+            <LabelSelectWithInput ref="selectLabelTool" v-model="FilterInspection.Tool" :name="langsInspection.Tools"
+                items="tools" itemFilter='code' :default="-1" :placeholder="langsInspection.PlaceHolder">
+            </LabelSelectWithInput>
+            <SelectLabel ref="selectLabelStatus" v-model="FilterInspection.Status" :name="langsInspection.Status.Text"
+                :items="langsInspection.Status.Options" valueItem="id" />
+
+        </div>
+        <div class="flex gap-3">
+            <ButtonIcon icon="fa-solid fa-magnifying-glass" text="" pad="p-3" bg="bg-blue-600" space="normal" />
+            <ButtonIcon icon="fa-solid fa-trash" @click.stop.prevent="resetValues" text="" pad="p-3" space="normal" />
+        </div>
+
+    </form>
+</template>
+
+<script>
+import LabelSelectWithInput from '../../forms/LabelSelectWithInput.vue';
+import ButtonIcon from '../../widgets/ButtonIcon.vue';
+import { langStore } from '../../../store/langStore'
+import SelectLabel from '../../forms/SelectLabel.vue';
+export default {
+    data() {
+        return {
+            tools: [],
+            FilterInspection: {
+                Tool: '',
+                Status: '',
+
+            }
+        };
+    },
+    computed: {
+        langsInspection() {
+            return langStore().getLang.PageInspections.Filters
+        },
+    },
+    methods: {
+        async resetValues() {
+            this.FilterInspection.Tool = "";
+            this.FilterInspection.Status = "";
+            this.$refs['selectLabelTool'].resetValues();
+            this.$refs['selectLabelStatus'].resetValues();
+            // await toolsStore().doSearch(this.FilterTool, true);
+        }
+    },
+    components: {
+        LabelSelectWithInput,
+        ButtonIcon,
+        SelectLabel
+    }
+}
+</script>
