3da778db93322665dd5a962b221151e93887c41c
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-16 16:57:02 +0100

    PROJFINAL-194 added prevent to buttons

--- src/components/forms/LabelPickOptions.vue
+++ src/components/forms/LabelPickOptions.vue
@@ -2,9 +2,9 @@
     <div class="flex flex-col gap-2">
         <Label :name="name" :msg="name"/>
         <div class="flex gap-10 justify-around">
-            <ButtonIcon @click="changeValue(1)" class="flex flex-col items-center  hover:bg-blue-600" :bg="this.value == 1 ? 'bg-blue-600' : 'bg-zinc-300' "
+            <ButtonIcon @click.stop.prevent="changeValue(1, $event)" class="flex flex-col items-center  hover:bg-blue-600" :bg="this.value == 1 ? 'bg-blue-600' : 'bg-zinc-300' "
                 :text="text[0]" icon="fa-solid fa-thumbs-up"></ButtonIcon>
-            <ButtonIcon @click="changeValue(0)" class="flex flex-col items-center   hover:bg-MeivRed"  :bg="this.value == 0 ? 'bg-MeivRed' : 'bg-zinc-300' "
+            <ButtonIcon @click.stop.prevent="changeValue(0, $event)" class="flex flex-col items-center   hover:bg-MeivRed"  :bg="this.value == 0 ? 'bg-MeivRed' : 'bg-zinc-300' "
                 :text="text[1]" icon="fa-solid fa-thumbs-down"></ButtonIcon>
                 <Label v-if="isError" :name="name" class="font-openSans text-xs" color="text-red-500" :msg="msg"/> 
         </div>
@@ -29,15 +29,12 @@ export default {
         }
     },
     methods:{
-        changeValue(value){
+        changeValue(value, e ){
+            // e.stopPropagation()
+            console.log(value)
             this.value = value
             this.$emit('update:modelValue', value)
         },
-        inputValid(){
-            if(!this.value)
-                return this.isError = true;
-            return this.isError = false;
-        }
     },
     components: { Label, ButtonIcon }
 }
