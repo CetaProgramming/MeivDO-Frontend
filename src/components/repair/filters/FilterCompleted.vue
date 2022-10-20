3387e35eeee5f1ed17e1f3a62da33c770d4a35f5
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-20 22:02:54 +0100

    PROJFINAL-245 added logic to make filter responsive to both tables

--- src/components/repair/filters/FilterCompleted.vue
+++ src/components/repair/filters/FilterCompleted.vue
@@ -1,19 +1,20 @@
 <template>
     <form class="bg-white items  grid lg:grid-cols-1fr-auto gap-3 p-5 rounded-md dark:bg-MeivMatteBlack dark:text-white"
-        @submit.prevent="doSearch">
+        @submit.prevent="doSearch(false)">
         <div class="grid lg:grid-cols-2 gap-3">
             <LabelSelectWithInput ref="selectLabelTool" v-model="FilterRepair.Tool" :name="langsRepair.Tools"
-                items="tools" itemFilter='code' :default="-1" :placeholder="langsRepair.PlaceHolder">
+                :items="tools" itemFilter='code' :default="-1" :placeholder="langsRepair.PlaceHolder">
             </LabelSelectWithInput>
-            <SelectLabel ref="selectLabelStatus" v-model="FilterRepair.Status" :name="langsRepair.Status.Text"
-                :items="langsRepair.Status.Options" valueItem="id" />
+            <!-- <SelectLabel ref="selectLabelStatus" v-model="FilterRepair.Status" :name="langsRepair.Status.Text"
+                :items="langsRepair.Status.Options" valueItem="id" /> -->
+            <InputSearch ref="inputSearchInspection" v-model="FilterRepair.Inspection"
+                :name="langsRepair.KeywordInspection" />
 
         </div>
         <div class="flex gap-3">
             <ButtonIcon icon="fa-solid fa-magnifying-glass" text="" pad="p-3" bg="bg-blue-600" space="normal" />
             <ButtonIcon icon="fa-solid fa-trash" @click.stop.prevent="resetValues" text="" pad="p-3" space="normal" />
         </div>
-
     </form>
 </template>
 
@@ -22,15 +23,22 @@ import LabelSelectWithInput from '../../forms/LabelSelectWithInput.vue';
 import ButtonIcon from '../../widgets/ButtonIcon.vue';
 import { langStore } from '../../../store/langStore'
 import SelectLabel from '../../forms/SelectLabel.vue';
+import InputSearch from '../../forms/InputSearch.vue';
+import { repairCompletedStore } from '../../../store/repairCompletedStore'
+import { repairMissingStore } from '../../../store/repairMissingStore'
 export default {
+    props: ['selectStore'],
     data() {
         return {
             tools: [],
             FilterRepair: {
                 Tool: '',
-                Status: '',
-
-            }
+                Inspection: '',
+            },
+            store: {
+                Completed: repairCompletedStore(),
+                Missing: repairMissingStore(),
+            },
         };
     },
     computed: {
@@ -38,19 +46,38 @@ export default {
             return langStore().getLang.PageRepairs.Filters
         },
     },
+    watch: {
+        async selectStore(value, oldValue) {
+            if (value != oldValue){
+                this.resetValues()
+                await this.getdata()
+            } 
+        }
+    },
+    async mounted() {
+        await this.getdata()
+    },
     methods: {
+        async getdata() {
+            const data = await this.store[this.selectStore].getTools()
+            this.tools = data;
+        },
+        async doSearch(reset = false) {
+            await this.store[this.selectStore].doSearch(this.FilterRepair, reset);
+        },
         async resetValues() {
             this.FilterRepair.Tool = "";
-            this.FilterRepair.Status = "";
+            this.FilterRepair.Inspection = "";
             this.$refs['selectLabelTool'].resetValues();
-            this.$refs['selectLabelStatus'].resetValues();
-            // await toolsStore().doSearch(this.FilterTool, true);
+            this.$refs['inputSearchInspection'].resetValues();
+            await this.doSearch(true)
         }
     },
     components: {
         LabelSelectWithInput,
         ButtonIcon,
-        SelectLabel
+        SelectLabel,
+        InputSearch
     }
 }
 </script>
