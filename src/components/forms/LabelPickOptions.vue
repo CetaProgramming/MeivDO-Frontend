2b90e1842556294c85eb1b9f12ed82a270d2828b
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-21 09:41:18 +0100

    PROJFINAL-262 removed hover on buttons

--- src/components/forms/LabelPickOptions.vue
+++ src/components/forms/LabelPickOptions.vue
@@ -3,9 +3,9 @@
         <Label :name="name" :msg="name"/>
         <div class="flex flex-col gap-3 justify-around">
             <div class="flex gap-10">
-                <ButtonIcon @click.stop.prevent="changeValue(1, $event)" class="flex flex-col items-center text-black dark:text-white  hover:bg-blue-600" :bg="this.value == 1 ? 'bg-blue-600' : 'bg-zinc-300 dark:bg-zinc-700' "
+                <ButtonIcon @click.stop.prevent="changeValue(1, $event)" class="flex flex-col items-center text-black dark:text-white " :bg="this.value == 1 ? 'bg-blue-600' : 'bg-zinc-300 dark:bg-zinc-700' "
                 :text="text[0]" icon="fa-solid fa-thumbs-up"></ButtonIcon>
-                <ButtonIcon @click.stop.prevent="changeValue(0, $event)" class="flex flex-col items-center text-black dark:text-white  hover:bg-MeivRed"  :bg="this.value == 0 ? 'bg-MeivRed' : 'bg-zinc-300 dark:bg-zinc-700' "
+                <ButtonIcon @click.stop.prevent="changeValue(0, $event)" class="flex flex-col items-center text-black dark:text-white"  :bg="this.value == 0 ? 'bg-MeivRed' : 'bg-zinc-300 dark:bg-zinc-700' "
                 :text="text[1]" icon="fa-solid fa-thumbs-down"></ButtonIcon>
             </div>
             
