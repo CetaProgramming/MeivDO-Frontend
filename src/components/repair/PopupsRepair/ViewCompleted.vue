961bcd439171d721b3a264c1e4c9d6c447f96708
bmgcosta <bmgcosta@outlook.com>
2022-11-02 18:30:09 +0000

    PROJFINAL-295 Fix inspection id

--- src/components/repair/PopupsRepair/ViewCompleted.vue
+++ src/components/repair/PopupsRepair/ViewCompleted.vue
@@ -4,7 +4,7 @@
             class="font-openSans grid grid-cols-1 items-center gap-5 md:gap-1  lg:grid-cols-1fr-auto md:justify-between ">
             <div class="flex flex-col gap-2 lg:w-4/5">
                 <LabelShowInfo :header="langs.Tool" :info="formGroupRepairCompletedShow.tool"></LabelShowInfo>
-                 <LabelShowInfo :header="langs.Inspection" :info="formGroupRepairCompletedShow.description"></LabelShowInfo>
+                 <LabelShowInfo :header="langs.Inspection" :info="formGroupRepairCompletedShow.inspection"></LabelShowInfo>
                  <LabelShowInfo :header="langs.Reason" :info="formGroupRepairCompletedShow.reason"></LabelShowInfo>
                  <LabelShowInfo :header="langs.Solution" :info="formGroupRepairCompletedShow.solution"></LabelShowInfo>
                  <LabelShowInfo :header="langs.Description" :info="formGroupRepairCompletedShow.description"></LabelShowInfo>  
@@ -25,7 +25,7 @@ export default {
             groupToolsStore: inspectionCompletedStore(),
             formGroupRepairCompletedShow: {
                 tool: this.value.tool.code,
-                inspection: this.value.inspection.additionalDescription,
+                inspection: this.value.inspection.id,
                 reason: this.value.reason,
                 solution: this.value.solution,
                 description: this.value.description
